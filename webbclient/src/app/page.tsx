3e3f7a57301a3a5b9c748b087f93b3412ee5a93d
Axel Andersson <axean602@student.liu.se>
2023-10-19 13:46:36 +0200

    Initial starting page and component structure

--- webbclient/src/app/page.tsx
+++ webbclient/src/app/page.tsx
@@ -1,5 +1,12 @@
+import Navigation from "@/components/Navigation";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col items-center justify-center">
+        <h1>Welcome</h1>
+        <Navigation />
+      </div>
+    </main>
   );
 }
---
+++ webbclient/src/components/Navigation.tsx
@@ -0,0 +1,11 @@
+const Navigation = () => {
+  return (
+    <div className="flex flex-row justify-between w-80 mt-10">
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+    </div>
+  );
+};
+
+export default Navigation;
